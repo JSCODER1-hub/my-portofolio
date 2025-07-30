795845e774cf16f338c59dd4893e157b0fc2ad88
mohamed medhat <73649080+CoDeBrEaKe@users.noreply.github.com>
2025-07-31 00:29:17 +0300

    enhanced the website

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div id="contact" className='contact'>
-        <h3>That's All About Me, Feel Free<br/> To Say Hi! </h3>
-        <p>Catch Up with me, <br/>I am available to work just mail me at working time (8 a.m : 5 p.m) I'm very<br/> welcome for everyone.</p>
-        <a href='mailto:mohamedmedhat86452@yahoo.com' className='contact-button'>Contact Me</a>
+    <div id="contact" className="contact">
+      <h3>
+        That's All About Me, Feel Free
+        <br /> To Say Hi!{" "}
+      </h3>
+      <p>
+        Catch Up with me, <br />I am available to work just mail me at working
+        time (8 a.m : 5 p.m) I'm very
+        <br /> welcome for everyone.
+      </p>
+      <a href="mailto:mohamedmedhat86452@gmail.com" className="contact-button">
+        Contact Me
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone} from '@fortawesome/free-solid-svg-icons'
-import { faFacebookF ,faLinkedin , faGithub , faYahoo} from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faLinkedin,
+  faGithub,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer id='about'>
+    <footer id="about">
       <div>
-      <p>Copyright &copy; 2021 Mohamed All Rights Reserved</p>
+        <p>Copyright &copy; 2021 Mohamed All Rights Reserved</p>
       </div>
-      <div className='flex'>
-
-        <div className='links'>
-        <a href='https://www.facebook.com/mohamedte7a22/' ><FontAwesomeIcon  className='face' icon={faFacebookF} /></a>
-        <a href="https://www.linkedin.com/in/mohamed-medhat-236575230/"><FontAwesomeIcon className='linked' icon={faLinkedin} /></a>
-        <a href="https://github.com/CoDeBrEaKe"><FontAwesomeIcon className='git' icon={faGithub} /></a>
-      </div>
-      <div className='phone'>
-
-        <FontAwesomeIcon className='phone' icon={faPhone} /> <p>+201112269700</p>
-      </div>
-      <div className='mail'>
-
-        <FontAwesomeIcon className='yahoo' icon={faYahoo} /> <p>mohamedmedhat86452@yahoo.com</p>
-      </div>
-
+      <div className="flex">
+        <div className="links">
+          <a href="https://www.facebook.com/mohamedte7a22/">
+            <FontAwesomeIcon className="face" icon={faFacebookF} />
+          </a>
+          <a href="https://www.linkedin.com/in/mohamed-medhat-236575230/">
+            <FontAwesomeIcon className="linked" icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/CoDeBrEaKe">
+            <FontAwesomeIcon className="git" icon={faGithub} />
+          </a>
+        </div>
+        <div className="phone">
+          <FontAwesomeIcon className="phone" icon={faPhone} />{" "}
+          <p>+201112269700</p>
+        </div>
+        <div className="mail">
+          <FontAwesomeIcon className="yahoo" icon={faGoogle} />{" "}
+          <p>mohamedmedhat86452@gmail.com</p>
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
